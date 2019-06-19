de1d680d3883e1f393e56f0d560b602c758d417e
DandilionLau <incredibleup@gmail.com>
2019-06-19 10:42:58 -0700

    rename Account->AccountApi in OpenAPI to resolve conflict

--- src/main/java/com/yahoo/identity/services/key/KeyService.java
+++ src/main/java/com/yahoo/identity/services/key/KeyService.java
@@ -3,6 +3,7 @@ package com.yahoo.identity.services.key;
 import javax.annotation.Nonnull;
 
 public interface KeyService {
+
     @Nonnull
     String getSecret();
 
--- src/main/java/com/yahoo/identity/services/key/KeyServiceImpl.java
+++ src/main/java/com/yahoo/identity/services/key/KeyServiceImpl.java
@@ -3,16 +3,21 @@ package com.yahoo.identity.services.key;
 import javax.annotation.Nonnull;
 
 public class KeyServiceImpl implements KeyService {
+
     private String secret;
 
-    public KeyServiceImpl(){
+    public KeyServiceImpl() {
         secret = "toySecret";
     }
 
     @Override
     @Nonnull
-    public String getSecret() { return secret; }
+    public String getSecret() {
+        return secret;
+    }
 
     @Override
-    public void setSecret(@Nonnull String secret) { this.secret = secret;}
+    public void setSecret(@Nonnull String secret) {
+        this.secret = secret;
+    }
 }
