482117e591e6e761725330d39e79f814e42f0a41
sleepingpig <dotinthewhite@outlook.com>
2019-07-13 00:41:44 +0800

    Refine the way of handling error. (#10)

--- unit_test.go
+++ unit_test.go
@@ -6,41 +6,36 @@ import (
 	"testing"
 )
 
-
-
 func TestCreateAccount(t *testing.T) {
-	resp, err := http.PostForm("http://localhost:5000/create/", url.Values{"username":{"Henry123"},"firstname":{"Henry"},"lastname":{"Zhang"},"email":{"henryzhang@gmail.com"},"description":{"this is a new account"},"verified":{"true"},"password":{"PASSWORD"}})
-	if err != nil && resp.StatusCode% 100 != 2{
+	resp, err := http.PostForm("http://localhost:5000/create/", url.Values{"username": {"Henry123"}, "firstname": {"Henry"}, "lastname": {"Zhang"}, "email": {"henryzhang@gmail.com"}, "description": {"this is a new account"}, "verified": {"true"}, "password": {"PASSWORD"}})
+	if err != nil && resp.StatusCode%100 != 2 {
 		t.Fail()
 	}
 }
 func TestLogin(t *testing.T) {
-	resp, err := http.PostForm("http://localhost:5000/login/", url.Values{"username":{"Henry123"},"password":{"PASSWORD"}})
-	if err != nil && resp.StatusCode% 100 != 2{
+	resp, err := http.PostForm("http://localhost:5000/login/", url.Values{"username": {"Henry123"}, "password": {"PASSWORD"}})
+	if err != nil && resp.StatusCode%100 != 2 {
 		t.Fail()
 	}
 }
 
 func TestAccessProfile(t *testing.T) {
-	link := "http://localhost:5000/privatePage/"
+	link := "http://localhost:5000/accounts/@me"
 	resp, err := http.Get(link)
-	if err != nil && resp.StatusCode% 100 != 2{
+	if err != nil && resp.StatusCode%100 != 2 {
 		t.Fail()
 	}
 }
 func TestEdit(t *testing.T) {
 	resp, err := http.PostForm("http://localhost:5000/save/Henry123", url.Values{"email": {"henry123@yahoo.com"}, "description": {"Hello from your new Email"}})
-	if err != nil && resp.StatusCode% 100 != 2{
+	if err != nil && resp.StatusCode%100 != 2 {
 		t.Fail()
 	}
 }
 
 func TestGetPublicProfile(t *testing.T) {
 	resp, err := http.Get("http://localhost:5000/accounts/Henry123")
-	if err != nil && resp.StatusCode% 100 != 2{
+	if err != nil && resp.StatusCode%100 != 2 {
 		t.Fail()
 	}
 }
-
-
-
