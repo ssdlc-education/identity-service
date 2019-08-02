72c0a54506a6c803db2c0a4203fa566f3e32d97a
sleepingpig <dotinthewhite@outlook.com>
2019-08-02 10:13:02 +0800

    Revert "Remove the solutions of the vulnerabilities (#64)"
    This reverts commit 0a492ed0db114e86f0ed6bcaa20c0cea26d34758.

---
+++ backend/src/main/java/com/verizonmedia/identity/services/token/TokenServiceImplFixed.java
@@ -0,0 +1,44 @@
+package com.verizonmedia.identity.services.token;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.verizonmedia.identity.services.key.KeyService;
+import com.verizonmedia.identity.services.system.SystemService;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import javax.annotation.Nonnull;
+
+public class TokenServiceImplFixed extends TokenServiceImpl {
+
+    private static final String KEY_NAME = "identity-token";
+
+    public TokenServiceImplFixed(@Nonnull KeyService keyService, @Nonnull SystemService systemService) {
+        super(keyService, systemService);
+    }
+
+    @Override
+    @Nonnull
+    Algorithm createAlgorithm(@Nonnull KeyService keyService) {
+        return Algorithm.RSA256((RSAPublicKey) keyService.getPublicKey(KEY_NAME, "RSA"),
+                                (RSAPrivateKey) keyService.getPrivateKey(KEY_NAME, "RSA"));
+    }
+
+    @Override
+    @Nonnull
+    public TokenCreate newTokenCreate() {
+        return new TokenCreateImplFixed(algorithm, systemService);
+    }
+
+    @Nonnull
+    Token createTokenFromJWT(@Nonnull DecodedJWT jwt) {
+        return new TokenImplFixed.Builder()
+            .setSystemService(systemService)
+            .setAlgorithm(algorithm)
+            .setSubject(jwt.getSubject())
+            .setIssueTime(jwt.getIssuedAt().toInstant())
+            .setExpireTime(jwt.getExpiresAt().toInstant())
+            .build();
+    }
+}
